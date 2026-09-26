23edc0ab55899e0032820c885d93d766af6b1c5e
CJ Lumanglas <justincarltz14@gmail.com>
2026-09-26 23:52:04 +0800

    Load sidebar icons on moderator schedules page

--- resources/views/moderator/schedules.blade.php
+++ resources/views/moderator/schedules.blade.php
@@ -4,6 +4,8 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Moderator Schedules - COMPASS</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;800;900&display=swap">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body class="compass-compact bg-gray-50">
